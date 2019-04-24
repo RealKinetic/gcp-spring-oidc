29541d5b6c1d0728759470eeb7329cbb50d25fed
Tyler Treat <ttreat31@gmail.com>
2019-04-23 21:21:21 -0500

    Use environment service account to authenticate
    When authenticating with an IAP-protected resource, this will first use
    service account credentials provided by the
    GOOGLE_APPLICATION_CREDENTIALS environment variable if present. If it's
    not present, it will use the service account provided by the runtime (e.g.
    GCE or App Engine) to authenticate.

--- src/main/java/com/realkinetic/gcp/spring/oidc/GCPAuthenticationInterceptor.java
+++ src/main/java/com/realkinetic/gcp/spring/oidc/GCPAuthenticationInterceptor.java
@@ -1,24 +1,31 @@
 package com.realkinetic.gcp.spring.oidc;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.api.client.http.*;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonObjectParser;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.GenericData;
+import com.google.auth.oauth2.ComputeEngineCredentials;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
+import org.jose4j.base64url.Base64;
+import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.MalformedClaimException;
+import org.jose4j.jwt.NumericDate;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
+import org.jose4j.jwt.consumer.JwtContext;
+import org.jose4j.lang.JoseException;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.lang.NonNull;
 
 import java.io.IOException;
-import java.security.interfaces.RSAPrivateKey;
 import java.util.Collections;
-import java.util.Date;
 
 /**
  * <p>A {@link org.springframework.web.client.RestTemplate} interceptor which can make HTTP requests to Google
@@ -36,12 +43,12 @@ public class GCPAuthenticationInterceptor implements ClientHttpRequestIntercepto
     private static final String IAM_SCOPE = "https://www.googleapis.com/auth/iam";
     private static final String OAUTH_TOKEN_URI = "https://www.googleapis.com/oauth2/v4/token";
     private static final String JWT_BEARER_TOKEN_GRANT_TYPE = "urn:ietf:params:oauth:grant-type:jwt-bearer";
-    private static final long EXPIRATION_TIME_IN_MILLIS = 3600 * 1000L;
+    private static final float EXPIRATION_TIME_IN_MINS = 60;
     private static final HttpTransport httpTransport = new NetHttpTransport();
 
     private final String clientId;
-    private final ServiceAccountCredentials credentials;
-    private DecodedJWT googleJwt;
+    private final GoogleCredentials credentials;
+    private JwtContext googleJwt;
 
     /**
      * Create a new interceptor which authenticates HTTP requests for the given OAuth2 client id.
@@ -51,7 +58,9 @@ public class GCPAuthenticationInterceptor implements ClientHttpRequestIntercepto
      */
     public GCPAuthenticationInterceptor(String clientId) throws IOException {
         this.clientId = clientId;
-        this.credentials = getCredentials();
+        this.credentials = GoogleCredentials
+                .getApplicationDefault()
+                .createScoped(Collections.singleton(IAM_SCOPE));
     }
 
     @Override
@@ -62,15 +71,15 @@ public class GCPAuthenticationInterceptor implements ClientHttpRequestIntercepto
             @NonNull ClientHttpRequestExecution execution) throws IOException {
 
         synchronized (this) {
-            if (googleJwt == null || googleJwt.getExpiresAt().before(new Date())) {
+            if (googleJwt == null || isExpired()) {
                 googleJwt = getGoogleIdToken();
             }
-            request.getHeaders().add("Authorization", "Bearer " + googleJwt.getToken());
+            request.getHeaders().add("Authorization", "Bearer " + googleJwt.getJwt());
         }
         return execution.execute(request, body);
     }
 
-    private DecodedJWT getGoogleIdToken() throws IOException {
+    private JwtContext getGoogleIdToken() throws IOException {
         String jwt = getSignedJwt();
         final GenericData tokenRequest = new GenericData()
                 .set("grant_type", JWT_BEARER_TOKEN_GRANT_TYPE)
@@ -86,33 +95,72 @@ public class GCPAuthenticationInterceptor implements ClientHttpRequestIntercepto
         HttpResponse response = request.execute();
         GenericData responseData = response.parseAs(GenericData.class);
         String idToken = (String) responseData.get("id_token");
-        return JWT.decode(idToken);
+        return decode(idToken);
     }
 
-    private ServiceAccountCredentials getCredentials() throws IOException {
-        GoogleCredentials credentials = GoogleCredentials
-                .getApplicationDefault()
-                .createScoped(Collections.singleton(IAM_SCOPE));
-
-        // Service account credentials are required to sign the jwt token.
-        if (credentials == null || !(credentials instanceof ServiceAccountCredentials)) {
-            throw new RuntimeException("Google credentials : service accounts credentials expected");
+    private boolean isExpired() {
+        try {
+            return googleJwt.getJwtClaims().getExpirationTime().isBefore(NumericDate.now());
+        } catch (MalformedClaimException e) {
+            return true;
         }
-        return (ServiceAccountCredentials) credentials;
     }
 
-    private String getSignedJwt() {
-        long now = System.currentTimeMillis();
-        RSAPrivateKey privateKey = (RSAPrivateKey) credentials.getPrivateKey();
-        Algorithm algorithm = Algorithm.RSA256(null, privateKey);
-        return JWT.create()
-                .withKeyId(credentials.getPrivateKeyId())
-                .withIssuer(credentials.getClientEmail())
-                .withSubject(credentials.getClientEmail())
-                .withAudience(OAUTH_TOKEN_URI)
-                .withIssuedAt(new Date(now))
-                .withExpiresAt(new Date(now + EXPIRATION_TIME_IN_MILLIS))
-                .withClaim("target_audience", clientId)
-                .sign(algorithm);
+    private JwtContext decode(String jwt) throws IOException {
+        JwtConsumer jwtConsumer = new JwtConsumerBuilder()
+                .setExpectedAudience(clientId)
+                .setSkipSignatureVerification()
+                .build();
+        try {
+            return jwtConsumer.process(jwt);
+        } catch (InvalidJwtException e) {
+            throw new IOException(e);
+        }
+    }
+
+    private String getSignedJwt() throws IOException {
+        JwtClaims claims = new JwtClaims();
+        claims.setAudience(OAUTH_TOKEN_URI);
+        claims.setIssuedAtToNow();
+        claims.setExpirationTimeMinutesInTheFuture(EXPIRATION_TIME_IN_MINS);
+        claims.setClaim("target_audience", clientId);
+
+        String jwt;
+        if (credentials instanceof ServiceAccountCredentials) {
+            // ServiceAccountCredentials indicates credentials provided through GOOGLE_APPLICATION_CREDENTIALS, which
+            // means we have the key to sign the JWT ourselves.
+            ServiceAccountCredentials creds = (ServiceAccountCredentials) credentials;
+            claims.setIssuer(creds.getClientEmail());
+            claims.setSubject(creds.getClientEmail());
+            JsonWebSignature jws = new JsonWebSignature();
+            jws.setPayload(claims.toJson());
+            jws.setKey(creds.getPrivateKey());
+            jws.setKeyIdHeaderValue(creds.getPrivateKeyId());
+            jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_USING_SHA256);
+            try {
+                jwt = jws.getCompactSerialization();
+            } catch (JoseException e) {
+                throw new IOException(e);
+            }
+        } else if (credentials instanceof ComputeEngineCredentials) {
+            // ComputeEngineCredentials indicates we're running in a managed environment (e.g. GCE or App Engine), which
+            // means we do not have the key to sign the JWT ourselves. Use the IAM signBlob API to sign it instead.
+            ComputeEngineCredentials creds = (ComputeEngineCredentials) credentials;
+            claims.setIssuer(creds.getAccount());
+            claims.setSubject(creds.getAccount());
+            String claimsJson = claims.toJson();
+            JwtClaims headers = new JwtClaims();
+            headers.setClaim("typ", "JWT");
+            headers.setClaim("alg", AlgorithmIdentifiers.RSA_USING_SHA256);
+            String headersJson = headers.toJson();
+            // JWT consists of "<b64(headers)>.<b64(claims)>".
+            String payload = Base64.encode(headersJson.getBytes()) + "." + Base64.encode(claimsJson.getBytes());
+            byte[] signature = creds.sign(payload.getBytes());
+            // Signed JWT consists of "<b64(headers)>.<b64(claims)>.<b64(signature)>".
+            jwt = payload + "." + Base64.encode(signature);
+        } else {
+            throw new RuntimeException("Google credentials: service accounts credentials expected");
+        }
+        return jwt;
     }
 }
